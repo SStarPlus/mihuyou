3b946ceff40ae54b9c8b875138dde7a3dcfa357e
JQ1691290121 <jq1691290121@icloud.com>
2022-03-08 17:53:30 +0800

    JavaEEPlus

---
+++ src/main/java/javaeepp/mihuyou/MihuyouApplication.java
@@ -0,0 +1,13 @@
+package javaeepp.mihuyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MihuyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MihuyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javaeepp/mihuyou/MihuyouApplicationTests.java
@@ -0,0 +1,13 @@
+package javaeepp.mihuyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MihuyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
