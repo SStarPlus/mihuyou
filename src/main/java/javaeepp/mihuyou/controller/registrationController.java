02f89053eebc1646d8017435d49225f3706b86a7
looperss <qq2324144106@163.com>
2022-03-13 13:42:24 +0800

    1
    update

---
+++ src/main/java/javaeepp/mihuyou/controller/registrationController.java
@@ -0,0 +1,25 @@
+package javaeepp.mihuyou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class registrationController {
+    @RequestMapping("user_registration")
+    public String userRegistration(HttpServletRequest request, Model model){
+        if(request.getParameter("inputType").equals("返回")){
+            return "login";
+        }else if(request.getParameter("inputType").equals("确认")){
+            String[] infoNames = {"userName", "userPwd", "userNation", "userSex"};
+            String[] infos = new String[infoNames.length];
+            for (int i = 0; i < infos.length; i++) {
+                infos[i] = request.getParameter(infoNames[i]);
+            }
+
+        }
+        return "registrationPage";
+    }
+}
