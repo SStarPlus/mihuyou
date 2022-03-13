b8a3c6f1ccf0f0695b7783e57643a0b7367bb6b7
looperss <qq2324144106@163.com>
2022-03-13 14:38:57 +0800

    输入合法即可注册成功

--- src/main/java/javaeepp/mihuyou/controller/registrationController.java
+++ src/main/java/javaeepp/mihuyou/controller/registrationController.java
@@ -1,5 +1,7 @@
 package javaeepp.mihuyou.controller;
 
+import javaeepp.mihuyou.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class registrationController {
-    @RequestMapping("user_registration_func")
-    public String userRegistration(HttpServletRequest request, Model model){
+    @Autowired
+    RegistrationService registrationService;
+
+    @RequestMapping("/user_registration_func")
+    public String userRegistration(HttpServletRequest request){
         if(request.getParameter("inputType").equals("返回")){
             return "login";
         }else if(request.getParameter("inputType").equals("确认")){
-            String[] infoNames = {"userName", "userPwd", "userNation", "userSex"};
+//            将获取的信息整合
+            String[] infoNames = {"userName", "userPwd", "userSex", "userNation"};
             String[] infos = new String[infoNames.length];
             for (int i = 0; i < infos.length; i++) {
                 infos[i] = request.getParameter(infoNames[i]);
             }
-
+//            将信息传入service，并插入数据库
+            registrationService.userRegistration(infos);
+            return "login";
         }
+
         return "registrationPage";
     }
 }
